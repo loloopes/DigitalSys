2f214074534c76a7d51001a526bbf094277cf724
Gustavo <gus.lcampos38@gmail.com>
2023-06-16 13:40:53 -0300

    initial configuration

---
+++ first_project/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .base import env
+
+ADMINS = [("Camila Lopes", "mensagemparacamila@gmail.com")]
+
+# TODO add domain names of production server
+CSRF_TRUSTED_ORIGINS = []
